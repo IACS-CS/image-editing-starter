5550d72eb47119296aa5b732a3ab26ffbd1fb550
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-01-11 14:27:19 +0000

    fancy select

--- src/components/ImageFilterChooser.tsx
+++ src/components/ImageFilterChooser.tsx
@@ -1,30 +1,40 @@
+import React from "react";
+import Select from "react-select";
 import filters from "../filters";
-import React, { FC } from "react";
 import type { Filter } from "../types";
-// Define the props expected by the ImageFilterChooser component
+
 interface ImageFilterChooserProps {
   onInput: (selectedFilter: Filter | null) => void;
 }
 
 const ImageFilterChooser: React.FC<ImageFilterChooserProps> = ({ onInput }) => {
-  // This function will be called when a new option is selected
-  const updateSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedFilterName = event.target.value;
-    const selectedFilter = filters.find(
-      (filter) => filter.name == selectedFilterName
-    );
-    onInput(selectedFilter || null);
+  const options = filters.map((filter) => ({
+    value: filter,
+    label: filter.name,
+  }));
+
+  const handleSelectChange = (selectedOption) => {
+    onInput(selectedOption ? selectedOption.value : null);
+  };
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? "white" : "black",
+      padding: 20,
+    }),
+    // Add other custom styles if needed
   };
 
   return (
     <main>
-      <select onInput={updateSelect}>
-        {filters.map((filter) => (
-          <option key={filter.name} value={filter.name}>
-            {filter.name}
-          </option>
-        ))}
-      </select>
+      <Select
+        onChange={handleSelectChange}
+        options={options}
+        styles={customStyles}
+        isClearable
+        isSearchable
+      />
     </main>
   );
 };
