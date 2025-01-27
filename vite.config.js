852d1d056c957f1b73757462d8df62742c461dff
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-01-27 12:27:40 +0000

    fix path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // This is the key to deploy on GitHub Pages
   server: {
     host: '0.0.0.0',
   }
