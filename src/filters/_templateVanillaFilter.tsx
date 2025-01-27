50312e413efbdf464f6487302b3e82e8e8ccc84c
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-01-27 14:58:14 +0000

    --long blurb about myself (I had this here in case kids used this code in the APCSP create task last year)

--- src/filters/_templateVanillaFilter.tsx
+++ src/filters/_templateVanillaFilter.tsx
@@ -1,16 +1,3 @@
-/**
- * This starter code for image editing work was authored by
- * Tom Hinkle (https://www.tomhinkle.net) and is provided as is
- * for use by any students or teachers interested in doing basic
- * image filtering with TypeScript.
- *
- * This starter code was provided by my teacher.
- *
- * This "template" is designed to be used to create simple
- * filters without any options.
- *
- */
-
 import type { Filter, FilterFunction } from "../types";
 
 const filterImage: FilterFunction = (pixels, width, height) => {
