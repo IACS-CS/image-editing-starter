e7da6f9990a4e750255cf9430c31871313c334c1
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-01-27 13:52:47 +0000

    Use simplified image file

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -2,9 +2,30 @@ import React from "react";
 
 import type { ImageInfo } from "../types";
 export const ImageDisplay: React.FC<{ image: ImageInfo }> = ({ image }) => {
+
+
+  const getAttribution = (image: ImageInfo) => {
+    if (image.attribution && image.sourceLink) {
+      return (
+        <a href={image.sourceLink} target="_blank" rel="noreferrer">
+          {image.attribution}
+        </a>
+      );
+    } else if (image.attribution) {
+      return <span>{image.attribution}</span>;
+    } else if (image.sourceLink) {
+      return <a href={image.sourceLink}>Source</a>;
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <figure>
-      <figcaption className="bar">{image.attribution}</figcaption>
+    <figure>      
+      
+      <figcaption className="bar attribution">
+          {getAttribution(image)}        
+      </figcaption>
       <img src={image.url} />
     </figure>
   );
