48ccac13d26d75884c71bf001b305e1246879d07
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2026-01-21 15:36:22 +0000

    Add samples

--- src/filters/index.tsx
+++ src/filters/index.tsx
@@ -4,14 +4,19 @@ import { challengeOneFilter } from "./challenge1";
 import { challengeTwoFilter } from "./challenge2";
 import { challengeThreeFilter } from "./challenge3";
 import { ultimateChallenge } from "./challenge4";
-
+// Examples...
+import { gridFilter } from "./samples/grid";
+import { vignetteFilter } from "./samples/vignette";
 
 const filters: Filter[] = [
   challengeZero,
   challengeOneFilter,
   challengeTwoFilter,
   challengeThreeFilter,
-  ultimateChallenge
+  ultimateChallenge,
+  // Delete the ones below to remove examples from menu...
+  gridFilter,
+  vignetteFilter,
 ];
 
 export default filters;
